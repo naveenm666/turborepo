43a3b87b7b43e8fab06297e7ec0298fa04f48be2
naveenm666 <maddurunaveen666@gmail.com>
2024-09-16 13:39:43 +0530

    file embedding

---
+++ apps/frontend/components/type.tsx
@@ -0,0 +1,8 @@
+// import { CoreMessage, CoreSystemMessage, CoreUserMessage, CoreAssistantMessage, CoreToolMessage } from 'ai';
+
+// // Define a type that includes CoreMessage with additional properties
+// export type CustomMessage =
+//   | (CoreSystemMessage & { fileUrl?: string; })
+//   | (CoreUserMessage & { fileUrl?: string; })
+//   | (CoreAssistantMessage & { fileUrl?: string; })
+//   | (CoreToolMessage & { fileUrl?: string; });
